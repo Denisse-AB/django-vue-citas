fcdee7f9ec71d2ef9af3f1005e4acad8479f4b0f
Denisse Abreu <denisseabreu21@yahoo.com>
2021-07-05 15:20:00 -0400

    delete unused import

--- citas/urls.py
+++ citas/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import PostViewSet
-from citas import views
+
 
 router = routers.DefaultRouter()
 router.register(r'post', PostViewSet)
