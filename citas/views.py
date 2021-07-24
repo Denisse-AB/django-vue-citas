3df3214c338d48fa913a8ca17ebefdc5455a1c5a
Denisse Abreu <denisseabreu21@yahoo.com>
2021-07-24 15:15:56 -0400

    serializer response and decorators

--- citas/views.py
+++ citas/views.py
@@ -1,19 +1,26 @@
-from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotAllowed
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.template.loader import render_to_string
 from django.core.mail import send_mail
 
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .serializers import PostSerializers
 from .models import Post
 
+
+@method_decorator(login_required, name='serializer_class')
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-date')
     serializer_class = PostSerializers
 
+
+    @method_decorator(ensure_csrf_cookie)
     def create(self, request):
-        if request.method == 'POST':
+        if request.method == 'POST' and request.data['csrftoken']:
             serializer = PostSerializers(data=request.data)
             if serializer.is_valid():
                 input_email = request.data.get('email')
@@ -24,7 +31,7 @@ class PostViewSet(viewsets.ModelViewSet):
                 count = q.count()
                 # Cuatro citas por hora, four appointments per hour.
                 if count > 3:
-                    return JsonResponse(request.data, status=200)
+                    return Response(status=status.HTTP_200_OK, data=request.data)
                 else:
                     serializer.save()
                     # email
@@ -40,7 +47,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
                     send_mail(subject, html_message, from_email, [to], html_message=html_message)
 
-                    return JsonResponse(request.data, status=201)
+                    return Response(status=status.HTTP_201_CREATED, data=request.data)
 
-            return HttpResponseBadRequest(serializer.errors)
-        return HttpResponseNotAllowed()
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
